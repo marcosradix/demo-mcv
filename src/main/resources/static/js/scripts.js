e1544cddd03bc4bf0f6c8e19aa9f886e8e51df9c
marcosradix <marcosradix@gmail.com>
2018-08-11 13:24:34 -0300

    rotas adicionadas

---
+++ src/main/resources/static/js/scripts.js
@@ -0,0 +1,9 @@
+$(function() {
+			$('[data-toggle="popover"]').popover();
+		});
+		
+		$(document).ready(function(){
+		    $(".navbar-toggle").click(function(){
+		        $(".sidebar").toggleClass("sidebar-open");
+		    })
+});
