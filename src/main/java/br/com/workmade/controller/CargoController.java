919642edcfc771ed0896215673a296e1d6c5002a
marcosradix <marcosradix@gmail.com>
2018-08-11 15:23:38 -0300

    webjars funcionando

--- src/main/java/br/com/workmade/controller/CargoController.java
+++ src/main/java/br/com/workmade/controller/CargoController.java
@@ -5,13 +5,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/")
-public class HomeController {
-	
-	
-	@GetMapping
-	public String home() {
-		return "/home";
-	}
+@RequestMapping("/cargos")
+public class CargoController {
 
+	
+	@GetMapping("/cadastrar")
+	public String cadastrar() {
+		return "/cargo/cadastro";
+	}
+	
+	@GetMapping("/listar")
+	public String listar() {
+		return "/cargo/lista";
+	}
+	
 }
